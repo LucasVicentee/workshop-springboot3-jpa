86b0ab89c91b54100715e0e28efe5eb29608722a
LucasVicentee <lh201612@gmail.com>
2025-09-25 20:38:48 -0300

    Criando a camada de serviços e configurando os caminhos para a pesquisa pelo ID do usuário

---
+++ src/main/java/com/LucasVicentee/ProjetoWeb/services/UserService.java
@@ -0,0 +1,25 @@
+package com.LucasVicentee.ProjetoWeb.services;
+
+import com.LucasVicentee.ProjetoWeb.entities.User;
+import com.LucasVicentee.ProjetoWeb.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
